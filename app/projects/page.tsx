12db562e2605f587ac70be1798ef007b171496ce
jineshpb <89720519+jineshpb@users.noreply.github.com>
2024-05-28 22:35:42 +0530

    type correction attempt 1

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,6 +3,14 @@ import client from "@/tina/__generated__/client"
 import React from "react"
 
 export default async function ProjectListPage() {
-  const result = await client.queries.projectsConnection()
-  return <ProjectListPageComponent {...result} />
+  const result = await client.queries.projectsConnection({
+    variables: { relativePath: "" }, // Add the variables property here
+  })
+  return (
+    <ProjectListPageComponent
+      data={result.data}
+      variables={{ relativePath: "" }}
+      query={result.query}
+    />
+  )
 }
